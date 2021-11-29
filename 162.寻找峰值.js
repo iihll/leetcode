d28f5c6902380d73116a7f334371ca35b4ccecbb
duanqing.xing <duanqing.xing@brgroup.com>
2021-11-29 18:21:16 +0800

    162.寻找峰值

---
+++ 162.寻找峰值.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=javascript
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  let left = 0
+  let right = nums.length - 1
+  while(left < right) {
+    const mid = parseInt(left + (right - left) / 2)
+    if(nums[mid] > nums[mid + 1]) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+  return left
+}
+// @lc code=end
--- test.js
+++ test.js
@@ -5,42 +5,16 @@ class ListNode {
   }
 }
 
-const l1 = {
-  val: 1,
-  next: {
-    val: 3,
-    next: null
-  }
+let node1 = new ListNode(1, null)
+
+let head = new ListNode(0, node1)
+
+console.log(head)
+
+while(head.next) {
+  head = head.next
 }
 
-const l2 = {
-  val: 4,
-  next: {
-    val: 2,
-    next: null
-  }
-}
+console.log(head)
 
-var addTwoNumbers = function (l1, l2) {
-  let l1_str = String(l1.val)
-  let l2_str = String(l2.val)
 
-  while (l1.next) {
-    l1_str += l1.next.val
-    l1 = l1.next
-  }
-
-  while (l2.next) {
-    l2_str += l2.next.val
-    l2 = l2.next
-  }
-
-  let l1_num = Number(l1_str.split('').reverse().join(''))
-  let l2_num = Number(l2_str.split('').reverse().join(''))
-
-  console.log(l1_num, l2_num)
-
-  return l1_num + l2_num
-}
-
-console.log(addTwoNumbers(l1, l2))
