6bd9895bc7656fadc3d5a80574d9e404e1164538
duanqing.xing <duanqing.xing@brgroup.com>
2022-02-09 15:23:09 +0800

    69.x-的平方根

--- 69.x-的平方根.js
+++ 69.x-的平方根.js
@@ -9,8 +9,9 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-  return Math.floor(Math.sqrt(x))
-};
+var mySqrt = function (x) {
+  if(x === 0) return 0
+  let ans = Math.exp(0.5 * Math.log(x))
+  return (ans + 1) * (ans + 1) <= x ? ans + 1 : ans
+}
 // @lc code=end
-
